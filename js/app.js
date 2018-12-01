bb40bc86d018473e47f1588c923d752b4bb7e3d1
Tyler Taydus <tylertaydus4444@gmail.com>
2018-11-30 17:21:34 -1000

    1

--- js/app.js
+++ js/app.js
@@ -3,6 +3,24 @@
 
 /*1) to the navmenu items (Home, Location, Blog) that will show and hide the contents for that specific navmenu item.*/
 
+var navBox = document.getElementsByClassName('navi');
+
+for(i=0; i<navBox.length; i++){
+
+navBox[i].addEventListener('click', dropDown);
+
+}
+
+function dropDown(){
+    var childElem = this.querySelectorAll('.inner')[0];
+
+    if(childElem.style.display === 'block'){
+        childElem.style.display = 'none';
+    }else{
+        childElem.style.display = 'block';
+    }
+}
+
 
 /*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
